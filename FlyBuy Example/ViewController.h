33283025ab3b2758de073eb47f1da61d8df215c9
Michael Harper <mharper@standalonecode.com>
2019-01-30 11:19:15 -0800

    Initial Commit

---
+++ FlyBuy Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlyBuy Example
+//
+//  Created by Michael Harper on 1/30/19.
+//  Copyright © 2019 Radius Networks, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FlyBuy Example/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FlyBuy Example
+//
+//  Created by Michael Harper on 1/30/19.
+//  Copyright © 2019 Radius Networks, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
